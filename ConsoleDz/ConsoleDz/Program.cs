02111ae56404b3992ec94cbe3c25cf6a9ef2034b
dokk911 <76562544+dokk911@users.noreply.github.com>
2021-01-14 17:44:55 +0300

    Add files via upload

---
+++ ConsoleDz/ConsoleDz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. При помощи циклов вы можете повторять один и тот же код множество раз.
+            //Напишите простейшую программу, которая выводит указанное(установленное)
+            //пользователем сообщение заданное количество раз.
+
+            int i = 0;
+            Console.Write("Напишите фразу: ");
+            string text = Console.ReadLine();
+            Console.Write("Сколько раз повторять: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            while (i < quantity)
+            {
+                Console.WriteLine(text);
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
