f9dd408395b44a195fbb07faec667519d8b98ec0
dokk911 <76562544+dokk911@users.noreply.github.com>
2021-01-16 17:24:48 +0300

    Add files via upload

---
+++ ConsoleHomeWork/ConsoleHomeWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пользователь вводит число, найти все делители этого числа.
+
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; number > i; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleHomeWork/ConsoleHomeWork2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Найти количество разрядов числа. 2 - один, 34 - два, 234 - 3 и тд.
+
+            int score = 0;
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (number != 0)
+            {
+                number = number / 10;
+                score++;
+
+                if(number == 0)
+                {
+                    Console.WriteLine(score);
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
