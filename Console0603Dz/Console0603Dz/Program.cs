f2abb8ffe98bb4cc1ff91a0ced7ec1bd60b58be8
dokk911 <76562544+dokk911@users.noreply.github.com>
2021-03-06 18:11:38 +0300

    Add files via upload

---
+++ Console0603Dz/Console0603Dz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console0603Dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int number = int.Parse(Console.ReadLine());
+                Square(number);
+                Console.WriteLine();
+            }
+        }
+
+        public static int Square(int square)
+
+        {
+            square *= square;
+            Console.WriteLine(square);
+            return square;
+        }
+    }
+}
